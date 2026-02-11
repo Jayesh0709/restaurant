d865049439329fc0418848a3e7ed3d1f222d9e06
Jayesh0709 <gehlotjayesh65@gmail.com>
2026-02-11 09:46:23 +0530

    port connection

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const connect = async () => {
+    console.log(process.env.MONGODB_URL);
     await mongoose.connect(process.env.MONGODB_URL)
         .then(() => { console.log("MongoDB connected") })
         .catch((er) => console.log("Not connected"))
